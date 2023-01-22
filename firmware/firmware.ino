f6a94aae086ac31b78614710b10782452a909a35
Thomas Geissl <thomas.geissl@gmail.com>
2023-01-22 23:05:59 +0100

    sending out data via osc

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,10 +1,58 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
-
+#pragma once
+#include <Wire.h>
+#include <WiFi.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
-#include <Wire.h>
+#include <OSCMessage.h>
+#include "./credentials.h"
+
+unsigned int localPort = 8888;
+unsigned int outPort = 9011;
+
+int sendCount;
+int count;
+int lastEntry;
+IPAddress outIp(192, 168, 178, 255);
+WiFiUDP udp;
+
 
 Adafruit_MPU6050 mpu;
+
+void sendValues(float ax, float ay, float az, float gx, float gy, float gz) {
+  OSCMessage msgA("/flamingo/accel");
+  OSCMessage msgG("/flamingo/gyro");
+
+  msgA.add(gx);
+  msgA.add(gy);
+  msgA.add(gz);
+
+  udp.beginPacket(outIp, outPort);
+  msgA.send(udp);
+  udp.endPacket();
+  msgA.empty();
+
+  udp.beginPacket(outIp, outPort);
+  msgG.send(udp);
+  udp.endPacket();
+  msgG.empty();
+}
+
+void printValues(float ax, float ay, float az, float gx, float gy, float gz) {
+  Serial.print("Acceleration: ");
+  Serial.print(ax);
+  Serial.print(", ");
+  Serial.print(ay);
+  Serial.print(", ");
+  Serial.print(az);
+  Serial.print(", ");
+  Serial.print("Gyro: ");
+  Serial.print(gx);
+  Serial.print(", ");
+  Serial.print(gy);
+  Serial.print(", ");
+  Serial.print(gz);
+  Serial.println("");
+}
 
 void setup(void) {
   Serial.begin(115200);
@@ -25,33 +73,26 @@ void setup(void) {
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   Serial.println("");
   delay(100);
+
+
+  Serial.println(SECRET_SSID);
+
+  WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print('.');
+  }
+  Serial.println();
+  Serial.print("Success! Connected, IP Address is ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-
-  /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-
-  /* Print out the values */
-  Serial.print("AccelX:");
-  Serial.print(a.acceleration.x);
-  Serial.print(",");
-  Serial.print("AccelY:");
-  Serial.print(a.acceleration.y);
-  Serial.print(",");
-  Serial.print("AccelZ:");
-  Serial.print(a.acceleration.z);
-  Serial.print(", ");
-  Serial.print("GyroX:");
-  Serial.print(g.gyro.x);
-  Serial.print(",");
-  Serial.print("GyroY:");
-  Serial.print(g.gyro.y);
-  Serial.print(",");
-  Serial.print("GyroZ:");
-  Serial.print(g.gyro.z);
-  Serial.println("");
+  sendValues(a.acceleration.x, a.acceleration.y, a.acceleration.z, a.gyro.x, a.gyro.y, a.gyro.z);
+  printValues(a.acceleration.x, a.acceleration.y, a.acceleration.z, a.gyro.x, a.gyro.y, a.gyro.z);
 
   delay(10);
 }
